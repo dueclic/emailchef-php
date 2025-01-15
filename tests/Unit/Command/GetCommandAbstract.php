517870fe2d82c07dd1cc090c0e1a54799d9af02d
Andrea Debernardi <andrea@debbaweb.it>
2025-01-15 10:04:05 +0100

    github action done

--- tests/Unit/Command/GetCommandAbstract.php
+++ tests/Unit/Command/GetCommandAbstract.php
@@ -17,6 +17,8 @@ abstract class GetCommandAbstract extends TestCase {
         if (!$phpunit_api_email && !$phpunit_api_password) {
             $dotenv = \Dotenv\Dotenv::createImmutable(dirname(dirname(dirname(__DIR__))));
             $dotenv->load();
+            $phpunit_api_email = $_ENV['PHPUNIT_API_EMAIL'];
+            $phpunit_api_password = $_ENV['PHPUNIT_API_PASSWORD'];
         }
 
         $this->email = $phpunit_api_email;
